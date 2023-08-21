be0f7939efa8b8ebe72c17b66139b3743eec2704
danwycliff <danwycliff6769@gmail.com>
2023-08-21 23:00:28 +0300

    code

--- src/components/App.js
+++ src/components/App.js
@@ -2,18 +2,21 @@
 import React,{useEffect,useState} from "react";
 
 function App(){
-    const[image,setImage] = useState('')
+    const[image,setImage] = useState(null)
     useEffect(() => {
         fetch("https://dog.ceo/api/breeds/image/random")
           .then((response) => response.json())
-          .then((data) => setImage(data.image))
-      }, []);
+          .then(data => setImage(data.message));
+      },[]);
 
     return(
         <div>
-        <p>Loading...</p>
-        <image src={image} alt="A Random Dog"/>
+            {image ? 
+            (<img src={image} alt="A Random Dog"/>
+            ) : (
+            <p>Loading...</p>
+            )}
         </div>
     )
-}
+};
 export default App;
